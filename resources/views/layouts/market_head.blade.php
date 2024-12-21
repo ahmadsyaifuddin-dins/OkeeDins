edde98da2121dc06c000c914b9adc4471e8e250c
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2024-12-21 14:10:31 +0800

    mengatur dan menambahkan cdn sweet alert2

--- resources/views/layouts/market_head.blade.php
+++ resources/views/layouts/market_head.blade.php
@@ -12,18 +12,27 @@
     <meta name="keywords" content="">
     <meta name="description" content="">
 
+    <!-- Tambahkan script global -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('food_fusion/style.css') }}">
     <link rel="stylesheet" href="{{ asset('food_fusion/css/vendor.css') }}">
 
-
-
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap"
         rel="stylesheet">
-
 </head>
+
+<body>
+    @yield('content')
+
+    <!-- Tempatkan untuk script tambahan -->
+    @yield('scripts')
+</body>
+
+</html>
