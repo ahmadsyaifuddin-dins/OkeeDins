6262a0ce1ff5af33ebb0f889ad3d6575701ce54b
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2024-12-21 21:54:31 +0800

    mengunci halaman register dan login ketika si user sdh ter logged

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-
+use App\Http\Controllers\ProfileController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,7 +18,7 @@ use App\Http\Controllers\RegisterController;
 
 Route::get('/', function () {
     return view('market');
-});
+})->name('market');
 
 // Route untuk menampilkan form registrasi
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
@@ -36,3 +36,26 @@ Route::post('/login', [LoginController::class, 'login']);
 
 // Route untuk menampilkan dashboard admin
 Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+
+
+Route::middleware(['auth'])->prefix('pelanggan')->name('pelanggan.')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
+    Route::put('/profile', [ProfileController::class, 'update'])->name('profile.update');
+});
+
+// Mengunci Halaman Login dan Register ketika pengguna telah logged
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/register', [RegisterController::class, 'showRegisterForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+    // Route lain yang memerlukan autentikasi
+});
+
+
+// Route Logout
+Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
