152089c9a32892789360cd231973e3683e9791b3
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2024-12-23 21:44:50 +0800

    melakukan pengecekan nama admin siapa yg masuk akan tampil namanya (secara dinamis)

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -52,6 +52,7 @@
 
             <!-- right -->
             <div class="flex flex-row-reverse items-center">
+                {{-- @if (Auth::Check()) --}}
                 <!-- user -->
                 <div class="dropdown relative md:static">
                     <button class="menu-btn focus:outline-none focus:shadow-outline flex flex-wrap items-center">
@@ -59,7 +60,8 @@
                             <img class="w-full h-full object-cover" src="{{ asset('cleopatra/src/img/user.svg') }}">
                         </div>
                         <div class="ml-2 capitalize flex ">
-                            <h1 class="text-sm text-gray-800 font-semibold m-0 p-0 leading-none">Administrator</h1>
+                            <h1 class="text-sm text-gray-800 font-semibold m-0 p-0 leading-none">
+                                {{ Auth::user()->name }}</h1>
                             <i class="fad fa-chevron-down ml-2 text-xs leading-none"></i>
                         </div>
                     </button>
@@ -83,13 +85,20 @@
 
                         <hr>
                         <a class="px-4 py-2 block capitalize font-medium text-sm tracking-wide bg-white hover:bg-gray-200 hover:text-gray-900 transition-all duration-300 ease-in-out"
-                            href="#">
+                            href="#"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             <i class="fad fa-user-times text-xs mr-1"></i>
                             log out
                         </a>
+                        <form id="logout-form" action="{{ route('admin.logout') }}" method="POST"
+                            style="display: none;">
+                            @csrf
+                        </form>
+
                     </div>
                 </div>
                 <!-- end user -->
+                {{-- @endif --}}
             </div>
             <!-- end right -->
         </div>
