89362f3a6bec6223b525933155928cbd478fe23d
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2024-12-28 11:40:42 +0800

    menambahkan comment untuk ngeclear route

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@ use App\Http\Controllers\ProfileController;
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "web" middleware group. Make something great!
-|
+|  
 */
+//TODO: php artisan route:clear
 
 //! Route untuk halaman utama (market)
 Route::get('/', function () {
