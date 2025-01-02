ea86708d69c371fc119eb090c2000707fddfb78c
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-01-02 18:26:23 +0800

    salah logic

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -51,7 +51,7 @@ class RegisterController extends Controller
 
         $validator = validator($request->all(), [
             'name' => 'required',
-            'email' => 'required|email|unique:email',
+            'email' => 'required|email|unique:users,email', // Perbaikan di sini
             'password' => 'required|min:8|confirmed',
             'alamat' => 'required',
             'tgl_lahir' => 'required|date',
