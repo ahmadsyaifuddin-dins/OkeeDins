44294c1e13b44820cb68ef10892fecec6382a2b7
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-01-03 14:31:33 +0800

    deleted file

--- resources/views/pages/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-
-@section('title', 'Home - FoodFusion')
-
-@section('content')
-    <h1>Welcome to FoodFusion</h1>
-    <p>Discover delicious meals and beverages, tailored for you!</p>
-@endsection
