149ce42ca8dc0acc5ddb802033f7f3124f212b56
Ahmad Syaifuddin <77381720+ahmadsyaifuddin-dins@users.noreply.github.com>
2025-01-06 08:43:20 +0800

    Create OrderItems.php

---
+++ app/Models/OrderItems.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItems extends Model
+{
+    use HasFactory;
+
+    protected $table = 'order_items';
+
+    protected $fillable = [
+        'order_id',
+        'produk_id',
+        'quantity',
+        'price',
+        'discount',
+        'subtotal'
+    ];
+
+    public function produk() // Sesuaikan nama relasi
+    {
+        return $this->belongsTo(Produk::class, 'produk_id');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
